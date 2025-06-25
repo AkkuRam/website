3346415f5270947ddcdcff5a5f4c94412836426d
AkkuRam <91744052+AkkuRam@users.noreply.github.com>
2025-06-25 23:35:48 +0200

    small

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
     st.sidebar.title("Navigation")
     st.sidebar.write("Use the sidebar to explore different sections of this app.")
 
-    if st.button("Click Me"):
-        st.success("Button clicked! Welcome to the app.")
+    # if st.button("Click Me"):
+    #     st.success("Button clicked! Welcome to the app.")
 
     name = st.text_input("What's your name?")
     if name:
